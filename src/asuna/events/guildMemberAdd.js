83e2bb0f308b21cc4e0bd7c4ee83bfb0d22a00be
Jade3375 <floodlockgames9@gmail.com>
2021-07-04 23:16:19 +0100

    removed eris fleet from package.json and added beta version of welcome messages

---
+++ src/asuna/events/guildMemberAdd.js
@@ -0,0 +1,77 @@
+
+module.exports = class {
+    constructor (client) {
+        this.client = client;
+    }
+
+    async run (guild, member) {
+        if(!member) return;
+        let server = await this.client.db.getRow("server", guild.id)
+        console.log(server.data)
+        if(!server.data || server.data == null) return
+        if(server.data.welcomeToggle == false) return
+        let channel = await guild.channels.get(server.data.welcomeChannel)
+        let members = guild.memberCount + ""
+
+        let welcomemsg = server.data.welcomeMessage.replace("{server}", guild.name).replace("{atuser}", `<@${member.id}>`)
+
+        console.log(welcomemsg)
+
+        switch (members.substr(-1)) {
+            case "1":
+                members = "st"
+                break;
+            case "2":
+                members = "nd"
+                break;
+
+            case "3":
+                members = "rd"
+                break;
+        
+            default:
+                members = "th"
+                break;
+        }
+
+        const { createCanvas, loadImage, registerFont } = require('canvas')
+        const fs = require('fs')
+
+        registerFont(__dirname + '/../fonts/Helvetica_Bold.ttf', {family: "Helvet", weight: "bold"})
+        registerFont(__dirname + '/../fonts/Helvetica_Medium.ttf', {family: "Helvet", weight: "medium"})
+
+        const canvas = createCanvas(500,200)
+        const ctx = canvas.getContext('2d');
+
+        loadImage("https://cdn.discordapp.com/attachments/704345807033204776/859900187745321030/trees.png").then(image => {
+            ctx.drawImage(image, 0,0)
+
+            ctx.font = 'bold 24px "Helvet"'
+            ctx.fillStyle = "white"
+            ctx.fillText(`Welcome ${member.username}!`, 160, 85)
+
+            ctx.font = 'medium 24px "Helvet"'
+            ctx.fillText(`you are the ${guild.memberCount}${members} member!`, 160, 115)
+
+            ctx.beginPath();
+            ctx.arc(100, 100, 48, 0, Math.PI * 2);
+            ctx.clip();
+
+            ctx.fillStyle = 'white';
+            ctx.fillRect(0, 0, 500,200)
+
+            ctx.beginPath();
+            ctx.arc(100, 100, 46, 0, Math.PI * 2);
+            ctx.clip();
+
+            loadImage(member.avatarURL).then(image => {
+                ctx.drawImage(image, 54, 54, 92, 92)
+                const buffer = canvas.toBuffer("image/png")
+                channel.createMessage(welcomemsg , {file: buffer, name: "welcome.png"})
+            })
+
+        })
+
+
+    }
+}
