5ca3c9e00e6a5402d6c933208bcd8b2f309fe751
Jade <floodlockgames9@gmail.com>
2021-08-26 21:09:54 +0100

    added warning for music removal

--- src/asuna/commands/music/play.js
+++ src/asuna/commands/music/play.js
@@ -35,6 +35,7 @@ class Play extends Command {
 
         let embed = new this.Embed()
         .setDescription(`[${search.info.title}](${search.info.uri}) has been added to the queue`)
+        .setFooter("due to recent developments in the music bot scene we will be removing music from the bot on the 30th of august")
         this.client.globalEmbedData(embed)
 
         message.channel.createMessage(embed.build())
