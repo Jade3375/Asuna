c1f3d4fc16ffa51f938a7106290e2822ceb53d8c
Jade3375 <36305507+Jade3375@users.noreply.github.com>
2021-10-01 21:27:40 +0100

    Revert

--- src/asuna/events/messageCreate.js
+++ src/asuna/events/messageCreate.js
@@ -11,7 +11,7 @@ module.exports = class {
         if(global.dev) {
             prefix = "a!"
         } else {
-            prefix = await this.client.pf.getPrefix(message.guildID).toLowerCase()
+            prefix = await this.client.pf.getPrefix(message.guildID)
         }
         
         let args = await message.content.slice(prefix.length).trim().split(' ');
