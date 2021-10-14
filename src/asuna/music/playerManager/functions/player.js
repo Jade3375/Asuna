98da31c18a73091d03bdadcbc8e672f7a86d00f4
Jade3375 <floodlockgames9@gmail.com>
2021-10-14 15:17:04 +0100

    honestly no clue if this actually works haha but yolo?

--- src/asuna/music/playerManager/functions/player.js
+++ src/asuna/music/playerManager/functions/player.js
@@ -87,7 +87,42 @@ class Player {
         player.skippers = new Map()
         let song = await player.queueManager.getSong()
         if(!song || song == undefined) return
-        player.play(song)
+        player.play(song).catch(e => {
+            this.client.getChannel(channelID).createMessage("an error occured with this song, skipping.....")
+            await player.queueManager.NextSong()
+            if(player.queueManager.queue == (null || undefined) ) {
+                let embed = new this.client.Embed()
+                .setDescription(`playback has ended, thanks for listening :heart:`)
+                this.client.globalEmbedData(embed)
+
+                let c = this.client.getChannel(channelID)
+                c.unsendMessage(this.lavaLink.manager.players.get(guildID).msg).catch(e => {
+                    console.log(e)
+                })
+                c.createMessage(embed.build()).catch(e => {
+                    console.log(e)
+                })
+                this.DisconnectPlayer(guildID, "END")
+                return
+            }
+            else { 
+                this.PlayTrack(guildID, channelID)
+                let song = await player.queueManager.getSong()
+                let embed = new this.client.Embed()
+                .addField('Now Playing', `[${song.info.title}](${song.info.uri})`)
+                this.client.globalEmbedData(embed)
+
+                let c = this.client.getChannel(channelID)
+                c.unsendMessage(this.lavaLink.manager.players.get(guildID).msg).catch(e => {
+                    console.log(e)
+                })
+                c.createMessage(embed.build()).then(m => {
+                    this.lavaLink.manager.players.get(guildID).msg = m.id
+                }).catch(e => {
+                    console.log(e)
+                })
+            }
+        })
 
         player.once("end", async () => {
             await player.queueManager.NextSong()
