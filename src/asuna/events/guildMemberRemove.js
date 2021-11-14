4a161e582ce308c8f844f8416ba4f1c9d1983305
Jade <floodlockgames9@gmail.com>
2021-11-14 23:44:03 +0000

    start work on member leave messages

---
+++ src/asuna/events/guildMemberRemove.js
@@ -0,0 +1,13 @@
+const { createCanvas, loadImage, registerFont } = require('canvas')
+
+module.exports = class {
+    constructor (client) {
+        this.client = client;
+    }
+
+    async run (guild, member, opt) {
+        if(guild.id != "415262181865553950") return
+        
+    }
+
+}
