8d71dbcc23089e4b3e1e1875ea3d3a8623109a9a
Jade <floodlockgames9@gmail.com>
2021-12-19 20:55:40 +0000

    added more porn for you horny fucks <3

---
+++ src/asuna/commands/nsfw/hneko.js
@@ -0,0 +1,32 @@
+let Command = require('../../structures/command');
+const fetch = require("node-fetch");
+
+module.exports = class extends Command {
+    constructor (client) {
+        super(client, {
+            name: "hneko",
+            description: "Lewds for all",
+            usage: "%hneko",
+            nsfw: true,
+            aliases: [ ]
+        });
+        this.client = client
+    }
+
+    async run(message, args) {
+
+        if (message.channel.nsfw){
+            fetch(`https://nekobot.xyz/api/image?type=hneko`)
+            .then(res => res.json())
+            .then(json => {
+                let responses = ['oh my thats lewd', 'ara ara', 'don\'t get all hot and bothered now']
+                let embed = new this.Embed()
+                .setColor("#FF69B4")
+                .setDescription(`<@${message.author.id}> enjoy. ${responses[Math.floor(Math.random() * responses.length)]}`)
+                .setImage(json.message)
+                this.client.globalEmbedData(embed)
+                message.channel.createMessage(embed.build())
+            })
+        }else message.channel.createMessage(":warning: This channel is not marked as NSFW :warning:")
+    }
+}
