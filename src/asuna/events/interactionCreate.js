ef8b32a6b1a1c7fac575e0c3736ac8ff03b7d607
Jade <floodlockgames9@gmail.com>
2021-12-27 01:39:06 +0000

    more robust handler for interactions

--- src/asuna/events/interactionCreate.js
+++ src/asuna/events/interactionCreate.js
@@ -4,6 +4,23 @@ module.exports = class {
     }
 
     async run (Interaction) {
-        if(this.client.commands.has(Interaction.data.name)) this.client.commands.get(Interaction.data.name).slash(Interaction)
+        
+        if(Interaction.member?.bot || Interaction.user?.bot) return;
+        if (this.client.db.ready == false) return;
+
+        let cmd = this.client.commands.get(Interaction.data.name)
+
+        if(cmd.conf.nsfw) {
+            if (await this.client.voteChecker.checkVote(message.author.id) == false) return Interaction.createMessage(`Vote for the bot and use all vote commands for 12 hours https://top.gg/bot/420907324945989632/vote`)
+
+            this.runSlash(cmd, Interaction)
+        }
+        this.runSlash(cmd, Interaction)
     }
+
+    runSlash(cmd, Interaction) {
+        cmd.slash(Interaction)
+        if(this.client.cmdlogger == true) console.log(`[   Command   ] [   ${Interaction.guildID}   ]   ${cmd} has been used.`)
+    }
+    
 }
