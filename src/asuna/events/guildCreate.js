0d55c7e0fc05693663ca51b4d1c1c7b726285ee1
kirancrooks <hello@kirancrooks.com>
2022-05-29 20:40:32 +0100

    Update guildCreate.js

--- src/asuna/events/guildCreate.js
+++ src/asuna/events/guildCreate.js
@@ -5,8 +5,7 @@ module.exports = class {
 
     async run (guild) {
         let logC = this.client.getChannel("422805308813869067")
-        //logC.createMessage(`[Joined Guild]\nName: ${guild.name}\nID: ${guild.id}`)
-        logC.createMessage("`[Joined Guild]\nName: ${guild.name}\nID: ${guild.id}`") // Quick test, aka sanity check, will most likely revert in 2 nanoseconds
+        logC.createMessage(`[Joined Guild]\nName: ${guild.name}\nID: ${guild.id}`)
         this.client.pf.getPrefix(guild.id)
     }
 }
